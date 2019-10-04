efcac80a16e02efe8fa6b65060ad25acea596603
AustinPotts <pottsevolvements@gmail.com>
2019-10-04 11:46:17 -0400

    Configured Parse Models + API Controller

---
+++ ios10-pokedex/Model Controllers/APIController.swift
@@ -0,0 +1,68 @@
+//
+//  APIController.swift
+//  ios10-pokedex
+//
+//  Created by Austin Potts on 10/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+class APIController {
+    
+    //Holder to append pokemon
+    var pokemonCollection: [Pokemon] = []
+    
+    
+    //Create the base URL from which the JSON will be parsed
+    let baseURL = URL(string: "https://pokeapi.co//api/v2/pokemon/")!
+    
+    
+    //Create a function that will take in the search term & a result of Pokemon to decode if no Error
+    func getPokemon(searchTerm: String, completion: @escaping(Result<Pokemon, Error>)-> Void ){
+        
+        //Append the search Term to the base url. Lowercased or else will not parse API correctly
+        let requestURL = baseURL.appendingPathComponent(searchTerm.lowercased())
+        
+        //Open up the URLSession to connect to API
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            
+            //Handle your errors
+            if let error = error {
+                NSLog("Error with data task: \(error)")
+                completion(.failure(error))
+                return
+            }
+            
+            //Make sure you have the data
+            guard let data = data  else {
+                NSLog("No data located")
+                completion(.failure(NSError()))
+                return
+            }
+            
+            
+        //Do, Try, Catch to Deocode the Data from JSON & Catch any errors
+            do {
+                let decoder = JSONDecoder()
+         //Converting any snake case JSON without enum
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+              let pokemonSearch = try decoder.decode(Pokemon.self, from: data)
+        //Printing the pokemon searched to test success
+                print(pokemonSearch)
+                completion(.success(pokemonSearch))
+                
+            } catch {
+                NSLog("Error decoding data: \(error)")
+                completion(.failure(error))
+            }
+            
+        }.resume()
+        
+    }
+    
+    
+    
+    
+}
---
+++ ios10-pokedex/Models/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  ios10-pokedex
+//
+//  Created by Austin Potts on 10/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+//The Model that will Parse the JSON
+struct Pokemon: Decodable {
+    let name: String
+    let id: Int
+    let sprite: Sprites
+    let type: [Type]
+}
+
+//Struct needed to 'Un-Nest Pokeom Image(sprite)
+struct Sprites: Decodable {
+    let frontDefault: URL
+    
+}
+
+//Two Structs needed to 'Un-Nest' the type of pokemon
+struct Type: Decodable, Equatable {
+    let type: Species
+}
+
+struct Species: Equatable, Codable {
+    let name: String
+}
